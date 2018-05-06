62d1e10e987e17b7e7a89d5811de80fca82d22be
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-06 21:00:44 +0200

    Correct js file

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
-var a = prompt('Provide a value'),
-    h = prompt('Provide b value'),
-    value = a * h / 2;
-
-alert('Your result is ' + value);
-
 function getTriangleArea(a, h) {
-   
-if ((a > 0) && ( h > 0)) {
-        console.log('Correct data');
-        return a * h / 2;
-    }
- 
-else {
-        console.log('Invalid data');
-    }
+    if (a <= 0 || h <= 0) {
+        console.log('Nieprawidłowe dane - wprowadz dodatnią wartośc');
+    } else {
+        var value = a*h/2;
+        return value;
+    } 
 }
 
 console.log(getTriangleArea(10, 6));
 
-var triangle1Area1 = getTriangleArea(20, 12);
-var triangle1Area2 = getTriangleArea(40, 24);
-var triangle1Area3 = getTriangleArea(80, 48);
+var triangle1Area1 = getTriangleArea(10, 15),
+    triangle1Area2 = getTriangleArea(15, 20),
+    triangle1Area3 = getTriangleArea(20, 25);
 
 console.log(triangle1Area1);
 console.log(triangle1Area2);
 console.log(triangle1Area3);
-    
